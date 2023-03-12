01e5ed29cd403225e1224b53bb529fd919735d1a
ajith <2k19cse005@kiot.ac.in>
2023-03-12 14:17:05 +0530

    Adding the numbers

---
+++ src/AddNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String args[]){
+        int a=2,b=3,c,sum;
+        sum = a+b;
+        c=a+b;
+        System.out.println("the sum"+c);
+
+    }
+
+}
